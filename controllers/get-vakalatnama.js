2073e707133ab1eeeac0120dcb89175826b2e24d
DevHiv3 <dev.hiv3e@gmail.com>
2025-06-09 18:55:17 +0530

    STATIC LINKS AND EDIT FUNCTIONALITY

--- controllers/get-vakalatnama.js
+++ controllers/get-vakalatnama.js
@@ -4,12 +4,11 @@ export default async function GetVakalatnamas(req,res){
 
     try {
         
-        const vakalatnamas = await Transaction.find({ phoneNumber: req.query.phoneNumber, type: "vakalatnama" }).sort({ timestamp: 1 })
+        const vakalatnamas = await Transaction.find({ phoneNumber: req.query.phoneNumber, type: "vakalatnama" }).sort({ timestamp: -1 })
         if (!vakalatnamas || vakalatnamas.length === 0) {
             return res.status(403).json({ message: 'No Vakalatnamas found' });
         }
-
-        console.log(vakalatnamas)
+        
         return res.status(200).json({ vakalatnamas: vakalatnamas, message: "success" });
 
     } catch(error){
--- controllers/pay-fee.js
+++ controllers/pay-fee.js
@@ -24,7 +24,7 @@ export default async function PayFee(req,res){
                 name, email, contact: String(phoneNumber)
             },
             notify: { email: true, sms: true },
-            callback_url: `https://bba-atwny.vercel.app/vakalatnama?phoneNumber=${phoneNumber}`,
+            callback_url: `https://bba-tawny.vercel.app/vakalatnama?phoneNumber=${phoneNumber}`,
             notes: {
                 email: email
             }
